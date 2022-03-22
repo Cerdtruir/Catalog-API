d35db4fbc812b345b6afdb1b40487269e28c8fbe
Cerdtruir <ghoornabeel@gmail.com>
2022-03-22 13:04:41 +0200

    Add modal popup with recipe details

--- src/index.js
+++ src/index.js
@@ -37,6 +37,40 @@ async function getLikeAPI() {
   return jsonResponse;
 }
 
+async function getRecipeInfo(id) {
+  const response = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`,
+  );
+  const jsonResponse = await response.json();
+  return jsonResponse.meals[0];
+}
+
+async function recipeSection(id) {
+  const mealDetails = await getRecipeInfo(id);
+  document.body
+    .querySelector('.recipe-details')
+    .classList.remove('recipe-details-hide');
+  document.body.querySelector('.recipe-modal-heading').innerText =
+    mealDetails.strMeal;
+  document.body.querySelector('.recipe-modal-image').src =
+    mealDetails.strMealThumb;
+  document.body.querySelector('.recipe-modal-instructions').innerText =
+    mealDetails.strInstructions;
+  document.body.querySelector('.recipe-modal-ingredients').innerHTML = '';
+  for (let i = 1; i < 20; i += 1) {
+    const ingredients = document.createElement('li');
+    const ingredient = `strIngredient${i}`;
+    const measurement = `strMeasure${i}`;
+    if (mealDetails[ingredient] === '') {
+      return;
+    }
+    ingredients.innerText = `${mealDetails[measurement]} ${mealDetails[ingredient]}`;
+    document.body
+      .querySelector('.recipe-modal-ingredients')
+      .append(ingredients);
+  }
+}
+
 async function generateHTML() {
   const page = document.getElementById('main');
   const likesArray = await getLikeAPI();
@@ -50,6 +84,12 @@ async function generateHTML() {
     const image = document.createElement('img');
     image.classList.add('recipe-image');
     image.src = meal.strMealThumb;
+
+    document.body.querySelector('.close-modal').onclick = () => {
+      document.body
+        .querySelector('.recipe-details')
+        .classList.add('recipe-details-hide');
+    };
 
     const textContainer = document.createElement('div');
     textContainer.classList.add('text-container');
